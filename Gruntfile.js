ec13447f1bc9c5b452dbea00dc30945884f6d7c0
Christopher Van <cvan@mozilla.com>
2014-03-25 04:23:38 -0700

    THIS IS MARKETPLACE-PERFBOARD

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function (grunt) {
+
+  var server = grunt.option('server');
+
+  var serverURLs = {
+    dev: 'https://marketplace-dev.allizom.org/',
+    prod: 'https://marketplace.firefox.com/',
+    stage: 'https://marketplace.allizom.org/'
+  };
+
+  if (!(server in serverURLs)) {
+    return console.error('Invalid server choice (valid choices: ' +
+                         Object.keys(serverURLs).join(', ') + ')');
+  }
+
+  console.log('Loading ' + server + ' <' + serverURLs[server] + '>\n');
+
+  grunt.initConfig({
+    phantomas: {
+      gruntSite: {
+        options: {
+          indexPath: './' + server + '/',
+          options: {
+            'post-load-delay': 5
+          },
+          url: serverURLs[server]
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-phantomas');
+
+  grunt.registerTask('default', ['phantomas']);
+};
